21cf32625f7aa2808d626d9e4ee6095b2176dc45
44East <antswert@gmail.com>
2023-09-04 12:54:30 +0300

    Init the project

---
+++ MedicalDeviceAPI/NatsComPortReader.cs
@@ -0,0 +1,54 @@
+﻿using NATS.Client;
+using System.IO.Ports;
+using System.Text;
+
+namespace MedicalDeviceAPI
+{
+    internal class NatsComPortReader
+    {
+        private readonly COMPort _comPort;
+        private readonly SerialPort _serialPort;
+        private readonly string _natsSubject;
+        private readonly IConnection _natsConnection;
+        public NatsComPortReader(COMPort comPort, string natsUrl, string natsSubject)
+        {
+            _comPort = comPort;
+            _serialPort = new SerialPort(comPort.Name, comPort.BaudRate, comPort.Parity, comPort.DataBits, comPort.StopBits);
+
+            _natsSubject = natsSubject;
+
+            Options options = ConnectionFactory.GetDefaultOptions();
+            options.Url = natsUrl; 
+
+            _natsConnection = new ConnectionFactory().CreateConnection(options);
+        }
+        public async Task StartReadingAsync()
+        {
+            using (SerialPort serialPort = _serialPort)
+            {
+                serialPort.Open();
+
+                Console.WriteLine("Listening for data from COM port...");
+
+                while (true)
+                {
+                    try
+                    {
+                        
+                        string data = serialPort.ReadLine();
+
+                       
+                        byte[] payload = Encoding.UTF8.GetBytes(data);
+                        _natsConnection.Publish(_natsSubject, payload); 
+
+                        Console.WriteLine($"Published data to NATS: {data}");
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Error reading from COM port: {ex.Message}");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ MedicalDeviceAPI/Program.cs
@@ -0,0 +1,21 @@
+﻿using MedicalDeviceAPI;
+using System.IO.Ports;
+
+internal class Program
+{
+    static async Task Main(string[] args)
+    {
+        var comPort = new COMPort()
+        {
+            Name = "COM9",
+            BaudRate = 9600,
+            Parity = Parity.None,
+            DataBits = 8,
+            StopBits = StopBits.One
+        };
+
+        var nats = new NatsComPortReader(comPort, "nats://localhost:4222", "mediсal_device");
+        await nats.StartReadingAsync();
+
+    }
+}
